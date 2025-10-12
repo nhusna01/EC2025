c2b8d3f4e99cce581dbf312d969be1304e2c6f91
nhusna01 <nrlhsn1310@gmail.com>
2025-10-12 16:19:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Genetic Algorithm"
+    page_title="Scientific Visualization"
 )
 
-st.header("Genetic Algorithm", divider="gray")
+st.header("Scientific Visualization", divider="gray")
 
