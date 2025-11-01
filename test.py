e322a39cdd8c4583a3e161a7f9be764c42342e02
nhusna01 <nrlhsn1310@gmail.com>
2025-11-01 14:40:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
+
 import streamlit as st
+from st_pages import Page, show_pages
 
+# Only call this ONCE
 st.set_page_config(
-    page_title="Student Survey"
+    page_title="Student Survey",
+    page_icon=":material/school:"
 )
-visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
 
+# Define your pages correctly
+show_pages([
+    Page("studentSurvey.py", title="Pencapaian Akademik Pelajar", icon=":material/school:"),
+    Page("StudentCoursework.py", title="Student Academic", icon=":material/book:")
+])
 
-st.set_page_config(
-    page_title="Study of AV Accident Survey Data"
-)
-visualise = st.Page('StudentCoursework.py', title='Student Academic', icon=":material/book:")
 
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
