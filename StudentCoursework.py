9e0282165ab78885a951a08add9df762a441bde1
nhusna01 <nrlhsn1310@gmail.com>
2025-11-01 22:25:17 +0800

    Update StudentCoursework.py

--- StudentCoursework.py
+++ StudentCoursework.py
@@ -1,13 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-# --- Streamlit Page Configuration ---
-st.set_page_config(page_title="AV Accident Survey Data", layout="wide")
-
 # --- CSV File Path (from GitHub Raw Link) ---
 csv_url = "https://raw.githubusercontent.com/nhusna01/EC2025/refs/heads/main/processed_av_accident_data.csv"
 
-st.title("AV Accident Data Viewer")
+# Set the title for the Streamlit app
+st.title("AV Accident Data Survey")
+
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
 
 try:
     # Load the CSV file from GitHub URL
