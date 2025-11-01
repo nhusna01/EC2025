982132ebe20060c0083bb5936e88427e3fbda5c3
nhusna01 <nrlhsn1310@gmail.com>
2025-11-01 14:36:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,12 @@ st.set_page_config(
 )
 visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
 
+
+st.set_page_config(
+    page_title="Study of AV Accident Survey Data"
+)
+visualise = st.Page('StudentCoursework.py', title='Student Academic', icon=":material/book:")
+
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
