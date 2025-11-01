79cd6ab7c6ee0455c749288b4fdf1b35f1ed2590
nhusna01 <nrlhsn1310@gmail.com>
2025-11-01 22:42:24 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ st.set_page_config(page_title="Dashboard", page_icon=":material/home:")
 # Sidebar Navigation Pages
 show_pages([
     Page("home.py", title="Homepage", icon=":material/home:"),
-    Page("studentSurvey.py", title="Student Survey", icon=":material/school:"),
+    Page("StudentSurvey.py", title="Student Survey", icon=":material/school:"),
     Page("StudentCoursework.py", title="Student Coursework", icon=":material/book:"),
-    Page("AV_Accident.py", title="AV Accident Survey", icon=":material/car:")
 ])
