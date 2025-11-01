f1598df4ff0ae76b8a6064b5842459625443b23b
nhusna01 <nrlhsn1310@gmail.com>
2025-11-01 22:26:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import streamlit as st
 from st_pages import Page, show_pages
 
 st.set_page_config(page_title="Student Survey", page_icon=":material/school:")
+st.set_page_config(page_title="AV Accident Survey Data", page_icon=":material/car:")
 
 show_pages([
     Page("studentSurvey.py", title="Pencapaian Akademik Pelajar", icon=":material/school:"),
