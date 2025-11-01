9e23cb6addb5215b1e0c40b41004703d4bb1812b
nhusna01 <nrlhsn1310@gmail.com>
2025-11-01 14:45:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,8 @@
 import streamlit as st
 from st_pages import Page, show_pages
 
-# Only call this ONCE
-st.set_page_config(
-    page_title="Student Survey",
-    page_icon=":material/school:"
-)
+st.set_page_config(page_title="Student Survey", page_icon=":material/school:")
 
-# Define your pages correctly
 show_pages([
     Page("studentSurvey.py", title="Pencapaian Akademik Pelajar", icon=":material/school:"),
     Page("StudentCoursework.py", title="Student Academic", icon=":material/book:")
